0e05270c2baf94b689861ee961da749b341b6c70
DESKTOP-C0HL1O1\npqhu <npqhung@gmail.com>
2022-06-25 12:20:47 +0800

    Add sample code

---
+++ src/main/java/my/boot/journey/JourneyApplication.java
@@ -0,0 +1,13 @@
+package my.boot.journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/boot/journey/JourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package my.boot.journey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
